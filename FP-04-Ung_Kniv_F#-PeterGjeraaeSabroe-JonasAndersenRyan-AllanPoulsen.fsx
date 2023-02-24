77b6078026bbb458d126ae1c303b6be4724428fb
Allan Poulsen <alpo@itu.dk>
2023-02-24 09:24:45 +0100

    Added suggested solutions to 4.3, 4.4 and 4.5

--- FP-04-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fsx
+++ FP-04-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fsx
@@ -22,17 +22,43 @@ let implodeRev (list:list<char>) =
         list
 
 // Exercise 4.3 - toUpper
-let toUpper (s:string) = failwith "not implemented"
+// let toUpper (s:string) = failwith "not implemented"
+//Allan's suggestion:
+let toUpper (s:string) = 
+    let d = explode s
+    let g = List.map System.Char.ToUpper d
+    implode g
+
 
 // explode >> map >> implode
-let toUpper1 (s:string) = failwith "not implemented"
+// let toUpper1 (s:string) = failwith "not implemented"
 // let toUpper2 (s:string) = implode (explode s |> List.map(fun x -> (Char.ToUpper(x))));;
-let toUpper2 (s:string) = failwith "not implemented"
+//Allan's suggestion
+let toUpper1 (s:string) = (explode >> List.map System.Char.ToUpper >> implode) s
+
+// let toUpper2 (s:string) = failwith "not implemented"
+// Allan's suggestion
+let toUpper2 (s:string) = s |> (implode << (explode >> List.map System.Char.ToUpper))
+
 
 // Exercise 4.4 - palindrome - treating empty strings as palindromes too.
-let rec palindrome (s:string) = failwith "not implemented"
+// let rec palindrome (s:string) = failwith "not implemented"
+// Allan's suggestion
+let palindrome (s:string) = 
+    let forward = toUpper1 s
+    let backward = s |> (implodeRev << (explode >> List.map System.Char.ToUpper))
+    if forward = backward 
+    then true
+    else false
+
 // Exercise 4.5 - ack
-let rec ack t = failwith "not implemented"
+// let rec ack t = failwith "not implemented"
+// Allan's suggestion
+let rec ack(m, n) = 
+    match m, n with
+    | 0, _ -> n + 1
+    | _, 0 -> ack((m-1), 1)
+    | _, _ -> ack((m-1), ack(m, (n-1)))
     // ack(3, 11) = 16381
 
 // Exercise 4.6 - time
