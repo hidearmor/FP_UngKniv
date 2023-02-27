b47c2be2bebd1b03fac44f2d8aa238116ebe8af0
Allan Poulsen <alpo@itu.dk>
2023-02-27 09:59:03 +0100

    Assignment - 4.1 to 4.6

--- FP-04-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fs
+++ FP-04-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fs
@@ -6,20 +6,17 @@ let explode (s:string) = List.ofArray(s.ToCharArray())
 let rec explode2 (s:string) = 
     match s with //does not work with "function" keyword, but does mit "match s with"
     | ""  -> []
-    | _ -> s.Chars(0)::explode2(s.Remove(0,1))
-
-
-
-
+    | _ -> s.Chars(0)::explode2(s.Remove(0,1))  //<-- s.Char(0) extracts first character, s.Remove(0,1) returns remaining string
 
 
 // Exercise 4.2 - implode - char list to string
+//Returns string build from list of char in same order as list, using foldback
 let implode (list:list<char>) = 
     List.foldBack 
         (fun (c:char) s -> string c + s)
         list
         ""
-
+//Returns string build from list of char in reverse order as list, using fold
 let implodeRev (list:list<char>) =
     List.fold 
         (fun s (c:char) -> string c + s)
@@ -29,48 +26,35 @@ let implodeRev (list:list<char>) =
 
 
 // Exercise 4.3 - toUpper
-//Jonas' One-liner
+//Here we explode s, using the above function. This list is fed into fun c -> System.Char.Upper which maps  it to a new list, that is then imploded.
 let toUpper (s:string) = implode(List.map (fun c -> System.Char.ToUpper c) (explode s))
 
-//Allan's suggestion:
-let toUpper (s:string) = 
-    let d = explode s
-    let g = List.map System.Char.ToUpper d
-    implode g
+// Alternative and more elaborate version:
+// let toUpper (s:string) = 
+//     let d = explode s
+//     let g = List.map System.Char.ToUpper d
+//     implode g
 
 
 
 // let toUpper1 (s:string) = failwith "not implemented"
-//Allan's suggestion
+//Receive s forward function it into explode, then List.Map etc . and subsequently implode it.
 let toUpper1 (s:string) = (explode >> List.map System.Char.ToUpper >> implode) s
 
-//Jonas' suggestion
-let toUpper1 (s:string) = 
-    // (List.map (fun c -> System.Char.ToUpper c) explode s) >> implode
-    explode >> List.map (fun c -> System.Char.ToUpper c) >> implode 
-    // but this one returns a function and not a value 
-    // - weren't we supposed to make it do the same as toUpper?
-
-
-
-// let toUpper2 (s:string) = failwith "not implemented"
-// Allan's suggestion
-let toUpper2 (s:string) = s |> (implode << (explode >> List.map System.Char.ToUpper))
-
-//Peter's suggestion
+//Pipe s into explode an "backwards" function forward through from explode to implode
 let toUpper2 (s:string) = s |> (implode << List.map System.Char.ToUpper << explode)
 
-//Jonas' suggestion
-let toUpper2 (s:string) =
-    explode s |> (implode << (List.map (fun c -> System.Char.ToUpper c)))
+// Alternative version
+// let toUpper2 (s:string) = s |> (implode << (explode >> List.map System.Char.ToUpper))
 
-
-
+// Alternative version 2
+// let toUpper2 (s:string) =
+//     explode s |> (implode << (List.map (fun c -> System.Char.ToUpper c)))
 
 
 // Exercise 4.4 - palindrome - treating empty strings as palindromes too.
-// let rec palindrome (s:string) = failwith "not implemented"
-// Allan's suggestion
+// Returns a boolean that is a comparison of the reesults of toUpper1 of s and the "backward" version of s
+// that comes through implodeRev
 let palindrome (s:string) = 
     let forward = toUpper1 s
     let backward = s |> (implodeRev << (explode >> List.map System.Char.ToUpper))
@@ -78,39 +62,33 @@ let palindrome (s:string) =
     then true
     else false
 
-//Jonas' suggestion
-// not a recursive implementation, but a simple and effective one
-let palindrome (s:string) = (s = (explode s |> implodeRev))
-
-//Peters suggestion
-let palindrome (s:string) =  s = (s |> (implode << List.rev << explode))
+// Alternative verrsion 1 (case sensitive)
+// let palindrome (s:string) = (s = (explode s |> implodeRev))
 
 
-
+// Alternative version 2 (case sensitive)
+// let palindrome (s:string) =  s = (s |> (implode << List.rev << explode))
 
 
 // Exercise 4.5 - ack
-// let rec ack t = failwith "not implemented"
-// Allan's suggestion
-let rec ack(m, n) = 
-    match m, n with
-    | 0, _ -> n + 1
-    | _, 0 -> ack((m-1), 1)
-    | _, _ -> ack((m-1), ack(m, (n-1)))
-
-//Peters suggestion
+// recursive ackermann function as per assignment with the patterm guards (when) that n and m must be higher than zero
 let rec ack t = 
     match t with
-    |(0,n) -> n+1
-    |(m,0) -> ack((m-1),1)
-    |(m,n) -> ack(m-1,ack(m, n-1)) 
-    
+    |(0,n) when n>0 -> n+1
+    |(m,0) when m>0 -> ack((m-1),1)
+    |(m,n) when m>0 && n>0 -> ack(m-1,ack(m, n-1)) 
+    | _ -> failwith "Fail, man!"
+
+// Alternative version
+//     match m, n with
+//     | 0, _ -> n + 1
+//     | _, 0 -> ack((m-1), 1)
+//     | _, _ -> ack((m-1), ack(m, (n-1)))
+  
 // ack(3, 11) = 16381
 
-
-
-
 // Exercise 4.6 - time
+// Function as per assignment:
 let time f =
     let start = System.DateTime.Now in
     let res = f () in
@@ -121,13 +99,10 @@ let time f =
     Ovenstående expression giver 00:00:01.0542130
 *)  
 
-//Peters suggestion
+//Allows us to use "time" function above as requested in assignment
 let timeArg1 f a = time (fun g -> f a)
 
-
-
-
-
+//-----
 // Exercise 4.7 - HR 5.4 - downTo2 f n e
 //Peters suggestion - ikke helt korrekt. Jeg ved ikke hvordan man skal lave den recursive.
 let rec downto1 f (n, e) = 
