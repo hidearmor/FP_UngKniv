22d1be228d2e2beed7ecfdbf0571600346fa0798
jrya <jrya@itu.dk>
2023-03-20 14:12:49 +0100

    6.1 change type to STACK

--- FP-06-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fsx
+++ FP-06-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fsx
@@ -32,6 +32,8 @@ let rec fexprToString expr =
 // stack is a list of floats
 type Stack = S of float list
 
+let stackExmpl = S[4.5;3.5]
+
 type Instruction = | ADD | SUB | MULT | DIV | SIN
                    | COS | LOG | EXP | PUSH of float
 
@@ -39,41 +41,42 @@ type Instruction = | ADD | SUB | MULT | DIV | SIN
 // type be like: intpInstr: Stack -> Instruction -> Stack 
 
 // open type System.Math
-let intpInstr (stack: float list) (ins: Instruction) = 
+let intpInstr (S stack) (ins: Instruction) = 
     match ins with
     // stack -> x:s:xs (hvordan gør vi der her?)
     | ADD -> 
         match stack with
-        | x0::x1::xs -> (x0 + x1)::xs
-        | _ -> stack
+        | x0::x1::xs -> (x0 + x1)::xs |> S
+        | _ -> stack |> S
     | SUB -> 
         match stack with
-        | x0::x1::xs -> (x0 - x1)::xs
-        | _ -> stack
+        | x0::x1::xs -> (x0 - x1)::xs |> S
+        | _ -> stack |> S
     | MULT -> 
         match stack with
-        | x0::x1::xs -> (x0 * x1)::xs
-        | _ -> stack
+        | x0::x1::xs -> (x0 * x1)::xs |> S
+        | _ -> stack |> S
     | DIV -> 
         match stack with
-        | x0::x1::xs -> (x0 / x1)::xs
-        | _ -> stack
+        | x0::x1::xs -> (x0 / x1)::xs |> S
+        | _ -> stack |> S
     | SIN -> 
         match stack with
-        | x0::xs -> (System.Math.Sin(x0))::xs
-        | _ -> stack
+        | x0::xs -> (System.Math.Sin(x0))::xs |> S
+        | _ -> stack |> S
     | COS -> 
         match stack with
-        | x0::xs -> (System.Math.Cos(x0))::xs
-        | _ -> stack
+        | x0::xs -> (System.Math.Cos(x0))::xs |> S
+        | _ -> stack |> S
     | LOG -> 
         match stack with
-        | x0::xs -> (System.Math.Log(x0))::xs
-        | _ -> stack
+        | x0::xs -> (System.Math.Log(x0))::xs |> S
+        | _ -> stack |> S
     | EXP -> 
         match stack with
-        | x0::xs -> (System.Math.Exp(x0))::xs
-        | _ -> stack
+        | x0::xs -> (System.Math.Exp(x0))::xs |> S
+        | _ -> stack |> S
+    | PUSH r -> r::stack |> S
     
 
 // let intpProg  = 0
